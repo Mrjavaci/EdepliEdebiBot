566f420297263670889f74a32d971eaa7c639e31
Osman Taha Kıyıcılar <tr.osmantaha@gmail.com>
2023-07-03 19:27:10 +0300

    Add Telegraph library

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            TelegraphSeeder::class,
             WordSeeder::class,
             SongSeeder::class,
             PoemsSeeder::class
