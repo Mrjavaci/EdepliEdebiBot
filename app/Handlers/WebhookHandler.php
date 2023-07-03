38f968e85c90b632a542a5ea69f24b7469ba422a
Osman Taha Kıyıcılar <tr.osmantaha@gmail.com>
2023-07-03 20:04:35 +0300

    Update Telegraph System.

--- app/Handlers/WebhookHandler.php
+++ app/Handlers/WebhookHandler.php
@@ -2,13 +2,27 @@
 
 namespace App\Handlers;
 
+use Illuminate\Support\Stringable;
+
 class WebhookHandler extends \DefStudio\Telegraph\Handlers\WebhookHandler
 {
     public function __call(string $name, array $arguments)
     {
         if (!class_exists(\Illuminate\Support\Str::studly($name . 'Command'))) {
-            $this->chat->html('<bold>Bahsi Geçen Komut Bulunamamıştır. Örnek komutlar aşağıdadır.</bold>');
+            $this->listAllCommands();
         }
         (new $name)->handleCommand();
     }
+
+    protected function listAllCommands()
+    {
+        $this->chat->html('<bold>Bahsi Geçen Komut Bulunamamıştır. Örnek komutlar aşağıdadır.</bold>');
+    }
+
+    protected function handleUnknownCommand(Stringable $text): void
+    {
+        $this->listAllCommands();
+    }
+
+
 }
