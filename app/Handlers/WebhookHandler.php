8eced6ec8b52a3416a521cdedb76fc14b476a0b1
Osman Taha Kıyıcılar <tr.osmantaha@gmail.com>
2023-07-03 20:10:47 +0300

    Update Telegraph System.

--- app/Handlers/WebhookHandler.php
+++ app/Handlers/WebhookHandler.php
@@ -2,26 +2,35 @@
 
 namespace App\Handlers;
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Stringable;
 
 class WebhookHandler extends \DefStudio\Telegraph\Handlers\WebhookHandler
 {
-    public function __call(string $name, array $arguments)
-    {
-        if (!class_exists(\Illuminate\Support\Str::studly($name . 'Command'))) {
-            $this->listAllCommands();
-        }
-        (new $name)->handleCommand();
-    }
-
-    protected function listAllCommands():void
-    {
-        $this->chat->html('<bold>Bahsi Geçen Komut Bulunamamıştır. Örnek komutlar aşağıdadır.</bold>')->send();
-    }
-
     protected function handleUnknownCommand(Stringable $text): void
     {
-        $this->listAllCommands();
+        Log::info('handleUnknownCommand');
+        $this->handleCustomCommand((string)$text->after('/')->before(' ')->before('@'), (string)$text->after('@')->after(' '));
+    }
+
+    /**
+     * @param string
+     * @return void
+     */
+    public function handleCustomCommand(string $name, $parameter): void
+    {
+        if (!class_exists(\Illuminate\Support\Str::studly($name . 'Command'))) {
+            Log::info('!class_exists');
+
+            $this->listAllCommands();
+        }
+        Log::info('class_exists');
+        (new $name)->handleCustomCommand($parameter);
+    }
+
+    protected function listAllCommands(): void
+    {
+        $this->chat->html('<bold>Bahsi Geçen Komut Bulunamamıştır. Örnek komutlar aşağıdadır.</bold>')->send();
     }
 
 
